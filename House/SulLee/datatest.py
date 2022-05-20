f66e461a482a2387c2a1364306a4f8a854f9a55d
SUL <ini2854@naver.com>
2022-05-20 20:20:39 +0900

    법정구 최대 7개구 불러오기 가능

---
+++ House/SulLee/datatest.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import requests
+from xml.etree import ElementTree as ET
+import xmltodict
+#  행정구별 코드
+df = pd.read_csv('../datas/법정동코드 조회자료.csv')
+print(df.head())
+dong=df['법정동코드'].to_list()
+print(dong[:3], type(dong))
+for i in dong:
+    
+    APIKEY = "zOVsayBFwURddA1RPDX0L2MzMSH9k7Ivls+AztXTeudlmP4d7Z/zL50JEdI4Eu82mhRBur/IYHJgb5GJR8jpzQ=="
+    URL = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade"
+    CODE = i
+    START = "2012-01"
+    END = "2021-12"
+    
+    data = []
+    for ymd in pd.date_range(START, END, freq="1M").strftime("%Y%m").to_list():
+        response = requests.get(
+            URL,
+            params={
+                'serviceKey' : APIKEY,
+                'LAWD_CD' : CODE,
+                'DEAL_YMD' : ymd
+            }
+        )
+        df = pd.DataFrame.from_dict(xmltodict.parse(ET.tostring(ET.fromstring(response.text)[1][0], encoding='unicode')).get('items').get('item'))
+        df['ymd'] = ymd
+        data.append(df)
+    data = pd.concat(data)
+print(data.info())
+    
+
+# new_df = pd.DataFrame(df, columns=['ymd','법정동','아파트','전용면적','거래금액'])
+# print(new_df.head(3))
+# new_df.to_csv('apt2.csv',encoding ='utf-8-sig')
---
+++ House/SulLee/test.py
@@ -0,0 +1,36 @@
+import json            
+import requests        
+import xmltodict       
+import pandas as pd    
+
+APIKEY = "janG8FZzVlFCaTco2h3fKdlm9%2B3nn1gOaLMRkjcM9CczaBaE%2B1rat9qQQTuTNKcP4KAyPYLM3Bn80l%2FRQkT6%2Bw%3D%3D"
+
+def get_data(lawd_cd, deal_ymd):
+    base_url = "http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?serviceKey=" +APIKEY
+    base_url += f'&LAWD_CD={lawd_cd}'
+    base_url += f'&DEAL_YMD={deal_ymd}'
+    res = requests.get(base_url)
+    data = json.loads(json.dumps(xmltodict.parse(res.text)))
+    # print(data)
+    df = pd.DataFrame(data['response']['body']['items']['item'])
+    # print(df.info)
+    return df
+
+# get_data(11680, 202101)
+
+seoul = [['강남구', '11680'], ['강동구', '11740'], ['강북구', '11305'], ['강서구', '11500'], ['관악구', '11620'], ['광진구', '11215'], ['구로구', '11530'], ['금천구', '11545'], ['노원구', '11350'], ['도봉구', '11320'], ['동대문구', '11230'], ['동작구', '11590'], ['마포구', '11440'], ['서대문구', '11410'], ['서초구', '11650'], ['성동구', '11200'], ['성북구', '11290'], ['송파구', '11710'], ['양천구', '11470'], ['영등포구', '11560'], ['용산구', '11170'], ['은평구', '11380'], ['종로구', '11110'], ['중구', '11140'], ['중랑구', '11260']]
+print(len(seoul))
+date = pd.date_range('20110101', '20211201', freq='MS').strftime('%Y%m')
+
+apt = pd.DataFrame()
+for name, code in seoul:
+    sgg = pd.DataFrame()
+    for ym in date:
+        temp = get_data(code, ym)
+        sgg = pd.concat([sgg, temp])
+    sgg['시군구명'] = name
+    apt = pd.concat([apt, sgg])
+apt['시도명'] = '서울특별시'
+print(apt.info())
+apt.to_csv('서울특별시 아파트 실거래가 201101~202112.csv', index=False)
+
