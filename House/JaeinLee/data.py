e669bd378659bc9761c569470a14ec4f47c1a242
Jaein <jain080@nate.com>
2022-05-24 19:31:28 +0900

    data.py 생성

---
+++ House/JaeinLee/data.py
@@ -0,0 +1,23 @@
+'''
+Created on 2022. 5. 24.
+@author: Jain
+'''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rc('font', family='malgun gothic')
+
+# 강남구합계, 시계열데이터 정보 불러오기
+apart = pd.read_csv('../datas/아파트2.csv', encoding='cp949', low_memory = False)
+print(apart.head(2))
+apart = apart[apart['구']=='강남구']
+apart = apart.loc[:,['거래금액','ymd']]
+print(apart)
+
+# 행정구별 인구수 불러오기
+people = pd.read_csv('../datas/행정구별_인구수.csv', encoding='cp949')
+print(people.head(2))
+
+# 통화량, cpi, 기대인플레 데이터 불러오기
+data = pd.read_csv('../datas/시간별 데이터 합.csv', encoding='cp949')
+print(data.head(2))
