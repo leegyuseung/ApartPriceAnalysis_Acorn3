d6b7d11e41cc946579191903552b7f5850a88926
leegyuseung <lgs4002@naver.com>
2022-05-29 23:01:52 +0900

    db connect, urls

--- django10kakaomap/django10kakaomap/settings.py
+++ django10kakaomap/django10kakaomap/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'django10kakaomap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'acorn',
+        'USER': 'acorn',
+        'PASSWORD': '1234',
+        'HOST': 'acorn3-1.cqjirndgiwgh.us-west-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
