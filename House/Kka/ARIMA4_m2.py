6e6333240cd0887d9bcf7674342c0cd0885223f8
Kyeonga <ruddken055@gmail.com>
2022-06-03 17:58:37 +0900

    ARIMA4로 m2 모델링까지 완료.

--- House/Kka/ARIMA4_m2.py
+++ House/Kka/ARIMA4_m2.py
@@ -9,38 +9,46 @@ warnings.filterwarnings("ignore")
 plt.style.use('ggplot')
 
 # 데이터 불러오기
-data = pd.read_csv('구별,월별 평당가격.csv', parse_dates=['ymd'])
-Jongro = pd.DataFrame()
+data = pd.read_csv('시간별 데이터 합.csv', parse_dates=['ymd'], encoding='cp949')
+m2 = pd.DataFrame()
 yymm = []
 yymm = pd.date_range("2011-01", "2022-01", freq="M")
-Jongro['yymm'] = yymm
-Jongro['price'] = data['종로구']
+m2['yymm'] = yymm
+m2['m2(%)'] = data['m2(%)']
 # Jongro.set_index('yymm', inplace=True)
-# print(Jongro.head(3), Jongro.tail(3), Jongro.shape)
-'''                   price                           price
-    yymm                           yymm
-    2011-01-31  1878.779446        2021-10-31  4057.966887
-    2011-02-28  1909.280698        2021-11-30  3687.941989
-    2011-03-31  1786.382560        2021-12-31  3907.407257    (132, 1)
-'''
-print(Jongro.info())
-timeSeries = Jongro.loc[:, ['yymm', 'price']]
+# print(m2.head(3), m2.tail(3), m2.shape)
+'''         yymm    m2                  yymm     m2
+    0 2011-01-31  6.49        129 2021-10-31  12.39
+    1 2011-02-28  4.95        130 2021-11-30  12.92
+    2 2011-03-31  4.33        131 2021-12-31  13.21    (132, 2)  '''
+
+
+print(m2.info())
+timeSeries = m2.loc[:, ['yymm', 'm2(%)']]
 timeSeries.index = timeSeries.yymm
 ts = timeSeries.drop("yymm", axis=1)
+'''             m2(%)
+    yymm             
+    2011-01-31   6.49
+    2011-02-28   4.95
+    ...           ...
+    2021-11-30  12.92
+    2021-12-31  13.21    [132 rows x 1 columns]  '''
 
 
-# # 2011-01부터 2021-12 까지 구별-월별 아파트 평당가격 그래프
+# # 2011-01부터 2021-12 까지 m2(%) 그래프
 # plt.figure(figsize=(15, 8))
 # plt.plot(ts)
-# plt.title("Seoul APT price 2011-01 ~ 2021-12")
+# plt.title("m2(%) 2011-01 ~ 2021-12")
 # plt.xlabel("Year-Month")
-# plt.ylabel("price")
+# plt.ylabel("m2(%)")
 # plt.show()
+
 
 
 # # seasonal_decompose()
 from statsmodels.tsa.seasonal import seasonal_decompose
-# result = seasonal_decompose(ts.price, model='additive')
+# result = seasonal_decompose(ts['m2(%)'], model='additive')
 # fig = plt.figure()
 # fig = result.plot()
 # fig.set_size_inches(15, 10)
@@ -53,23 +61,23 @@ from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 # ax1 = fig.add_subplot(211)
 # ax2 = fig.add_subplot(212)
 # fig = plot_acf(ts, ax = ax1)
-# ax1.set_title("ACF")
+# ax1.set_title("m2(%) ACF")
 # fig = plot_pacf(ts, ax = ax2)
-# ax2.set_title("PACF")
+# ax2.set_title("m2(%) PACF")
 # plt.show()
 
 
 # # 정상성 확인 : ADF(Augmented Dickey-Fuller test)
 # # 귀무가설 : 자료가 정상성을 만족하지 않는다. / 대립가설 : 자료가 정상성을 만족한다.
 from statsmodels.tsa.stattools import adfuller
-# result = adfuller(ts)
-# print('ADF Statistic : %f'% result[0])
-# print('p-value : %f'% result[1])
-# print('Critical Values : ')
-# for key, value in result[4].items():
-#     print('\t%s: %.3f'%(key, value))
-''' ADF Statistic : 3.745713
-    p-value : 1.000000
+result = adfuller(ts)
+print('ADF Statistic : %f'% result[0])
+print('p-value : %f'% result[1])
+print('Critical Values : ')
+for key, value in result[4].items():
+    print('\t%s: %.3f'%(key, value))
+''' ADF Statistic : 0.220353
+    p-value : 0.973380
     Critical Values : 
         1%: -3.487
         5%: -2.886
@@ -80,11 +88,11 @@ from statsmodels.tsa.stattools import adfuller
 ts_diff = ts - ts.shift()
 # plt.figure(figsize=(15, 8))
 # plt.plot(ts_diff)
-# plt.title("1st Differencing")
+# plt.title("m2(%) 1st Differencing")
 # plt.xlabel('Year-Month')
-# plt.ylabel('APT price')
+# plt.ylabel('m2(%)')
 # plt.show()
-# 1차 차분 데이터로 다시 정상성 검사
+# # 1차 차분 데이터로 다시 정상성 검사
 result = adfuller(ts_diff[1:])
 print('\n1차 차분 후')
 print('ADF Statistic : %f'% result[0])
@@ -92,12 +100,12 @@ print('p-value : %f'% result[1])
 print('Critical Values : ')
 for key, value in result[4].items():
     print('\t%s: %.3f'%(key, value))
-''' ADF Statistic : -8.329827
-    p-value : 0.000000
+''' ADF Statistic : -2.810977
+    p-value : 0.056723
     Critical Values : 
-        1%: -3.483
-        5%: -2.885
-        10%: -2.579            '''
+        1%: -3.487
+        5%: -2.886
+        10%: -2.580            '''
 
 
 # # 1차 차분 데이터로 ACF, PACF 그려서 ARIMA 모형의 p, q 결정
@@ -105,9 +113,9 @@ for key, value in result[4].items():
 # ax1 = fig.add_subplot(211)
 # ax2 = fig.add_subplot(212)
 # fig = plot_acf(ts_diff[1:], ax = ax1)
-# ax1.set_title("ACF_diff1")
+# ax1.set_title("ACF_m2(%) diff1")
 # fig = plot_pacf(ts_diff[1:], ax = ax2)
-# ax2.set_title("PACF_diff1")
+# ax2.set_title("PACF_m2(%) diff1")
 # plt.show()
 # # ==> ACF는 1 이후로 0에 수렴, PACF도 1 이후로 0에 수렴.
 
@@ -122,51 +130,34 @@ start_index = yymm[120] # 2021-01-31 00:00:00
 end_index = yymm[131]   # 2021-12-31 00:00:00
 forecast = model_fit.predict(start=start_index, end=end_index, typ='levels')
 # # 실제값이랑 비교하기
-# print(Jongro.tail(12))
+# print(m2.tail(12))
 # print(forecast)
-'''         - 실제값 -           - forecast -
-          yymm        price          price
-    2021-01-31  3496.207318    3575.980581
-    2021-02-28  3839.286391    3562.521393
-    2021-03-31  3042.478637    3708.155043
-    2021-04-30  3472.657944    3347.033717
-    2021-05-31  3360.089149    3308.235230
-    2021-06-30  3652.622963    3403.115084
-    2021-07-31  3464.264264    3540.810975
-    2021-08-31  3703.163257    3536.258541
-    2021-09-30  3869.608889    3611.851518
-    2021-10-31  4057.966887    3805.990202
-    2021-11-30  3687.941989    3985.972877
-    2021-12-31  3907.407257    3829.372543
+'''        - 실제값 -     - forecast -
+          yymm   m2(%)        m2(%)
+    2021-01-31  10.05      9.809443
+    2021-02-28  10.72     10.122215
+    2021-03-31  11.02     10.906094
+    2021-04-30  11.38     11.103326
+    2021-05-31  10.96     11.479991
+    2021-06-30  10.94     10.843344
+    2021-07-31  11.38     10.934445
+    2021-08-31  12.49     11.502211
+    2021-09-30  12.79     12.798305
+    2021-10-31  12.39     12.873326
+    2021-11-30  12.92     12.278899
+    2021-12-31  13.21     13.067209
 '''
 
-# # 시각화
+# 시각화
 # plt.figure(figsize=(15, 8))
-# plt.plot(Jongro.yymm, Jongro.price, label="original")
+# plt.plot(m2.yymm, m2['m2(%)'], label="original")
 # plt.plot(forecast, label='predicted')
-# plt.title("Jongro-gu APT price Forecast")
+# plt.title("m2(%) Forecast")
 # plt.xlabel("Year-Month")
-# plt.ylabel("APT price per 1.8m*1.8m")
+# plt.ylabel("m2(%)")
 # plt.legend()
 # plt.show()
 
-
-# # 잔차 분석 : 잘 안 됨...
-# 어떠한 패턴이나 특성이 없는지 확인!
-resi = np.array(Jongro[Jongro.yymm>=start_index].price) - np.array(forecast)
-# plt.figure(figsize=(15, 8))
-# plt.plot(Jongro[Jongro.yymm>=start_index], resi)
-# plt.xlabel("Year-Month")
-# plt.ylabel("Residual")
-# plt.legend()
-# plt.show()
-
-
-# # ACF 그래프 및 ADF 검정을 통해 정상성 확인
-# # ACF
-# fig = plt.figure(figsize=(15, 4))
-# fig = plot_acf(resi)
-# plt.show()
 
 
 # # 성능 확인
@@ -189,9 +180,9 @@ def score_check(y_true, y_pred):
     return df
 
 # score_check(np.array(Jongro[Jongro.yymm>=start_index].price), np.array(forecast))
-print(score_check(np.array(Jongro[Jongro.yymm>=start_index].price), np.array(forecast)))
-# ==>        R2   Corr     RMSE    MAPE
-# ==> 0  -0.777  0.366  268.541   6.086
+print(score_check(np.array(m2[m2.yymm>=start_index]['m2(%)']), np.array(forecast)))
+# ==>        R2   Corr   RMSE   MAPE
+# ==> 0  77.244  0.915  0.468  3.241
 
 
 
