46e679caba792400b4772e1229fe3756e3ab5a9f
leegyuseung <lgs4002@naver.com>
2022-06-10 17:34:13 +0900

    회원가입, 로그인, 로그아웃

--- ApartProject/users/urls.py
+++ ApartProject/users/urls.py
@@ -3,4 +3,8 @@ from users import views
 
 urlpatterns = [ 
     path('signup', views.signUp), 
+    path('signupok', views.signUpOk), 
+    path('login', views.login),  
+    path('loginok', views.loginOk), 
+    path('logout', views.logout),  
 ]  
