372ff30548eb9c754e6ff03a9426678959dc4fc6
SUL <ini2854@naver.com>
2022-06-10 15:31:44 +0900

    qna 구현중 Merge branch 'master' of https://github.com/xerathul/test333.git

--- ApartProject/qna/urls.py
+++ ApartProject/qna/urls.py
@@ -1,5 +1,16 @@
-'''
-Created on 2022. 6. 10.
+from django.urls import path
+from qna import views
 
-@author: u
-'''
+
+urlpatterns = [
+    path('list', views.listFunc),
+    path('insert', views.insertFunc),
+    path('insertok', views.insertOkFunc),
+    path('search', views.searchFunc),
+    path('content', views.contentFunc),
+    path('update', views.updateFunc),
+    path('updateok', views.updateOkFunc),
+    path('delete', views.deleteFunc),
+    path('deleteok', views.deleteOkFunc),
+    
+]
